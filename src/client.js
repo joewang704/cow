0f52a03ffe02f98bfdc2e6d717d12424743e87cd
joewang704 <joewang704@gmail.com>
2016-07-08 18:22:39 -0400

    Clean up code

--- src/client.js
+++ src/client.js
@@ -19,9 +19,9 @@ const store = createStore(
 if (module.hot) {
   // Enable Webpack hot module replacement for reducers
   module.hot.accept('./reducers', () => {
-    const nextRootReducer = require('./reducers/index').default;
-    store.replaceReducer(nextRootReducer);
-  });
+    const nextRootReducer = require('./reducers/index').default
+    store.replaceReducer(nextRootReducer)
+  })
 }
 
 render(
